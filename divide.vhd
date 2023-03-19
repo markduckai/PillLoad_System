770af30d2ee4238696c17397988233b90f39bcbb
markduckai <1453879902@qq.com>
2023-03-19 20:31:57 +0800

    modified:   divide.vhd

--- divide.vhd
+++ divide.vhd
@@ -3,4 +3,14 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
 
-ARCHITECTURE
+ENTITY divide IS
+    PORT (
+        cl1 : IN STD_LOGIC;
+        clk_divide : OUT STD_LOGIC
+    );
+END divide;
+
+ARCHITECTURE fuc_d OF divide IS
+BEGIN
+    
+END fuc_d;
