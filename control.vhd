d6ea3cf725fbba9ac00bc39b8a9e08e148194389
markduckai <1453879902@qq.com>
2023-05-23 17:19:07 +0800

    modified:   PillLoad_System.asm.rpt 	modified:   PillLoad_System.done 	modified:   PillLoad_System.fit.rpt 	modified:   PillLoad_System.fit.summary 	modified:   PillLoad_System.flow.rpt 	modified:   PillLoad_System.map.rpt 	modified:   PillLoad_System.map.summary 	modified:   PillLoad_System.pin 	modified:   PillLoad_System.pof 	modified:   PillLoad_System.qsf 	modified:   PillLoad_System.qws 	modified:   PillLoad_System.sim.rpt 	modified:   PillLoad_System.tan.rpt 	modified:   PillLoad_System.vhd 	modified:   PillLoad_System.vwf 	modified:   control.vhd 	modified:   db/PillLoad_System.(0).cnf.cdb 	modified:   db/PillLoad_System.(0).cnf.hdb 	modified:   db/PillLoad_System.(7).cnf.cdb 	modified:   db/PillLoad_System.(7).cnf.hdb 	modified:   db/PillLoad_System.asm.qmsg 	modified:   db/PillLoad_System.asm.rdb 	modified:   db/PillLoad_System.cmp.cdb 	modified:   db/PillLoad_System.cmp.hdb 	modified:   db/PillLoad_System.cmp.rdb 	modified:   db/PillLoad_System.cmp.tdb 	modified:   db/PillLoad_System.cmp0.ddb 	modified:   db/PillLoad_System.eco.cdb 	modified:   db/PillLoad_System.fit.qmsg 	modified:   db/PillLoad_System.hier_info 	modified:   db/PillLoad_System.hif 	modified:   db/PillLoad_System.lpc.html 	modified:   db/PillLoad_System.lpc.rdb 	modified:   db/PillLoad_System.lpc.txt 	modified:   db/PillLoad_System.map.cdb 	modified:   db/PillLoad_System.map.hdb 	modified:   db/PillLoad_System.map.qmsg 	modified:   db/PillLoad_System.pre_map.cdb 	modified:   db/PillLoad_System.pre_map.hdb 	modified:   db/PillLoad_System.rtlv.hdb 	modified:   db/PillLoad_System.rtlv_sg.cdb 	modified:   db/PillLoad_System.rtlv_sg_swap.cdb 	modified:   db/PillLoad_System.sgdiff.cdb 	modified:   db/PillLoad_System.sgdiff.hdb 	modified:   db/PillLoad_System.sim.cvwf 	modified:   db/PillLoad_System.sim.qmsg 	modified:   db/PillLoad_System.sim.rdb 	modified:   db/PillLoad_System.sld_design_entry.sci 	modified:   db/PillLoad_System.tan.qmsg 	modified:   db/PillLoad_System.tmw_info 	modified:   db/prev_cmp_PillLoad_System.asm.qmsg 	modified:   db/prev_cmp_PillLoad_System.fit.qmsg 	modified:   db/prev_cmp_PillLoad_System.map.qmsg 	modified:   db/prev_cmp_PillLoad_System.qmsg 	modified:   db/prev_cmp_PillLoad_System.sim.qmsg 	modified:   db/prev_cmp_PillLoad_System.tan.qmsg 	modified:   db/wed.wsf 	modified:   pillload_system.vhd.bak

--- control.vhd
+++ control.vhd
@@ -6,34 +6,39 @@ USE ieee.std_logic_unsigned.ALL;
 ENTITY control IS
     PORT (
         clr : IN STD_LOGIC; --清零信号
+        clk_alarm : IN STD_LOGIC; --报警信号
         judge : IN STD_LOGIC; --判断装瓶量是否超过最大瓶数
         en_cnt : OUT STD_LOGIC; --输出计数使能信号
         en_green : OUT STD_LOGIC; --绿灯显示信号
-        en_red : OUT STD_LOGIC --红灯显示信号
-        --方波闪烁报警//扬声器报警
+        en_red : OUT STD_LOGIC; --红灯显示信号
+        en_alarm : OUT STD_LOGIC--方波闪烁报警//扬声器报警
     );
 END control;
 
 ARCHITECTURE func_co OF control IS
-    SIGNAL tmp_cnt, tmp_green, tmp_red : STD_LOGIC := '0';
+    SIGNAL tmp_cnt, tmp_green, tmp_red, tmp_alarm : STD_LOGIC := '0';
 BEGIN
-    PROCESS (clr, judge)
+    PROCESS (clr, judge, clk_alarm)
     BEGIN
         IF (clr = '1') THEN --清零时重置输出端口参数
             tmp_cnt <= '1';
             tmp_green <= '1';
             tmp_red <= '0';
+            tmp_alarm <= '0';
         ELSIF (judge = '0') THEN --没到最大瓶数时使能有效，绿灯亮，红灯灭
             tmp_cnt <= '1';
             tmp_green <= '1';
             tmp_red <= '0';
-        ELSE --超过时使能无效，绿灯灭，红灯亮
+            tmp_alarm <= '0';
+        ELSE --超过时使能无效，绿灯灭，红灯亮,报警
             tmp_cnt <= '0';
             tmp_green <= '0';
             tmp_red <= '1';
+            tmp_alarm <= clk_alarm;
         END IF;
     END PROCESS;
     en_cnt <= tmp_cnt;
     en_green <= tmp_green;
     en_red <= tmp_red;
+    en_alarm <= tmp_alarm;
 END func_co;
